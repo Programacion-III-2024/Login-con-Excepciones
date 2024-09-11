125ac48163f9a38dbd5fd2f30d9e3f93e1db19d2
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2024-09-10 22:42:18 -0300

    Agregada api de logiN

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using CapaLogica;
+using API.Models;
+namespace API.Controllers
+{
+    public class PizzaController : ApiController
+    {
+
+
+        [Route("api/login")]
+        public IHttpActionResult Login(UserModel user)
+        {
+            Dictionary<string, bool> resultado = new Dictionary<string, bool>();
+
+            bool autenticacion = UsuarioController.Login(user.Username, user.Password);
+            resultado.Add("Resultado",autenticacion);
+
+            if(autenticacion)
+                return Ok(resultado);
+
+            return NotFound();
+        }
+
+    }
+}
